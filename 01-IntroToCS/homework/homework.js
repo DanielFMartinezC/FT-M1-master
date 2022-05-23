ff518980841afa857eafd975136e5178588f5ea7
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-05-23 14:49:08 -0500

    homework 1 solución

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,29 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let arBin= num.split('');
+  arBin = arBin.reverse();
+  for(let i = 0; i < arBin.length; i++) {
+    arBin[i] = arBin[i] * 2**i
+  }
+  let decimal = arBin.reduce((x, y) => x + y);
+  return decimal;
 }
-
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let binAr = []
+while(num >= 1) {
+  if(num % 2 === 0) {
+    binAr.push(0)
+  } else {
+    binAr.push(1)
+  };
+  num = num / 2
+};
+binAr.reverse();
+let bin = binAr.toString();
+let n = bin.replaceAll(',','');
+return n;
 }
 
 
