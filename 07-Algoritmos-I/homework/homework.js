a181da527bd945816abf1b5d4c8904d0e272e530
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-05-31 16:19:27 -0500

    Update homework.js

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,33 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  var iArr = [];
+  var solution = [1];
+  for(let i = 0; i < num; i ++){
+    iArr.push(i)
+  }
+  function esPrimo(numero) {
+    if(numero < 2) { return false };
+    if(numero === 2) { return true };
+    for(let i = 2; i < numero; i++) {
+      if(numero % i === 0) {
+        return false;
+      }
+    }
+    return true
+  }
+  let arrayPrimos = iArr.filter(x => esPrimo(x));
+  var numDiv = [num]  //[180, 90... 1]
+  for(let i = 0; numDiv[numDiv.length - 1] > 1; i++){
+    for(let j = 0; j < arrayPrimos.length; j++){
+      if(numDiv[i] % arrayPrimos[j] === 0){
+        solution.push(arrayPrimos[j]);
+        numDiv.push(numDiv[i] / arrayPrimos[j]);
+        break;
+      }
+    }
+  }
+  return solution
 }
 
 function bubbleSort(array) {
@@ -14,7 +40,6 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
 }
 
 
