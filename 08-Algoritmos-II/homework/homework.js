f43e9024373749692071788ad09c9c40ab2b398d
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-06-01 12:37:30 -0500

    Update homework.js

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,34 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if(array.length === 0){
+    return array
+  }
+  if(array.length === 1){
+    return array
+  } else {
+    var pivote = Math.ceil(Math.random() * ((array.length - 1) - 0) + 0);
+    var arrPivote = array[pivote]
+    var arrayLeft = [];
+    var arrayRigth = [];
+    for(let i = 0; i < pivote; i++){
+      if(array[i] < array[pivote]){
+        arrayLeft.push(array[i])
+      } else {
+        arrayRigth.push(array[i])
+      }
+    };
+    for(let i = pivote + 1; i < array.length; i++){
+      if(array[i] < array[pivote]){
+        arrayLeft.push(array[i])
+      } else {
+        arrayRigth.push(array[i])
+      }
+    };
+  }
+  array = [];
+  array = quickSort(arrayLeft).concat(arrPivote).concat(quickSort(arrayRigth))
+  return array
 }
 
 function mergeSort(array) {
@@ -14,7 +41,6 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
 }
 
 // No modificar nada debajo de esta línea
